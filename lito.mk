d7a9e198fe1317d1ceb68919b4b0fc185c37ebbf
Tim Zimmermann <tim@linux4.de>
2022-08-24 15:40:22 +0200

    sm7250-common: Inherit non_ab_device.mk
    * This is required to build applypatch for updating recovery
    
    Change-Id: I38053691f7724b5d6c30159c410d5d04ac136180

--- lito.mk
+++ lito.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
