9c4818d4cbcce1d199ff0c3fd564cd223987b9ab
Avanindra Ray <rayavanindra@gmail.com>
2023-02-08 21:14:08 +0530

    sm7250-common: Build QTI vibrator from source
    Signed-off-by: COSMIC <rayavanindra@gmail.com>

--- lito.mk
+++ lito.mk
@@ -411,7 +411,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service.xiaomi_lito
+    vendor.qti.hardware.vibrator.service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
