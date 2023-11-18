25420b2cca177e4f5fea8f591e2e9efe4bda9eea
Pascoato <viniciuspascoato@gmail.com>
2023-11-18 16:22:38 +0530

    sm7250-common: Build android.hardware.power@1.2 - Fixes: I hwservicemanager: getTransport: Cannot find entry android.hardware.power@1.2::IPower/default in either framework or device VINTF manifest.
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>
    Signed-off-by: JaswalAshish <ashish@m.ms.evolution-x.org>

--- lito.mk
+++ lito.mk
@@ -357,6 +357,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power@1.2.vendor \
     android.hardware.power-service.xiaomi-libperfmgr \
     android.hardware.power.stats@1.0-service.mock
 
