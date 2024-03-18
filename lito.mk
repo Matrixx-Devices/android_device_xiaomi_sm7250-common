81efcf1694cfc078ad1d0ae791e86cd73ac03a79
Alec Chan <alec3d02@gmail.com>
2024-03-18 08:57:25 +0800

    sm7250-common: build android.hardware.power.stats and add missing vintf *mute spamming in logcat

--- lito.mk
+++ lito.mk
@@ -328,7 +328,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr
+    android.hardware.power-service.xiaomi-libperfmgr \
+    android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
