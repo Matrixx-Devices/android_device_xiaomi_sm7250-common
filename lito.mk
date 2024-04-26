f214092d9810d87c15e892879a56de9f370787a0
Alec Chan <alec3d02@gmail.com>
2024-04-27 02:28:27 +0800

    Revert "sm7250-common: Add Livedisplay 2.1" *Switch to kernel based dimming to fix FOD This reverts commit 0c935a14fcde78b70efabc2c0a31c9b92c15f59e.

--- lito.mk
+++ lito.mk
@@ -278,10 +278,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
 
-# Livedisplay
+# LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.1-service.monet
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Lineage Health
 PRODUCT_PACKAGES += \
